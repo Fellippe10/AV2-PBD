189091cb3def9e5fd458ec80346295e5a8e73221
Fellippe10 <felipepalopolied@gmail.com>
2025-06-23 11:23:20 -0300

    Update and rename vagas.sql.txt to vagas.sql

---
+++ pbd/inserts/vagas.sql
@@ -0,0 +1,2 @@
+INSERT INTO Vagas (quarto_id, numero_vaga, descricao_peculiaridades, disponivel)
+VALUES (1, 1, 'Cama beliche inferior, perto da porta', 1); /* 1 para TRUE. Supondo que o ID do Quarto 101 seja 1 */
